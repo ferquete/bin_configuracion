cd0859ecde5607b02a3841271b697c877652d8ba
ferquete <fernando.real.cisneros@gmail.com>
2020-02-09 01:40:09 +0100

    {comentario}

---
+++ subirgit.py
@@ -0,0 +1,7 @@
+import os
+comentario = input("Comentario: ")
+
+if comentario:
+    os.system("git add -A && git commit -m \"{comentario}\" && git push -u origin master")
+else:
+    print("Error!")
