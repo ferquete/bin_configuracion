696dceaf622f979c9d861f7eaca87613759f8adf
ferquete <fernando.real.cisneros@gmail.com>
2020-02-10 22:28:39 +0100

    {comentario}

--- subir_github.py
+++ subir_github.py
@@ -1,22 +1,8 @@
 #!/usr/bin/python3
-#import os
-#comentario = input("Comentario: ")
-
-#if comentario:
-#    os.system("git add -A && git commit -m \"{comentario}\" && git push -u origin master")
-#else:
-#    print("Error!")
-import subprocess
 import os
-
-
 comentario = input("Comentario: ")
+
 if comentario:
-    os.system("git add -A && git commit -m \"{comentario}\"")
-    p = subprocess.run(["git","push","-u","origin","master"],
-                       stdout=subprocess.PIPE, 
-                       input=b"ferquete\n66qbcqbcGithub??\n")
-    print(p.stdout)
+    os.system("git add -A && git commit -m \"{comentario}\" && git push -u origin master")
 else:
-    print("Error! se necesita un comentario...")
-    exit(1)
+    print("Error!")
